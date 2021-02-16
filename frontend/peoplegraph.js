2051176ef9f4a76d99c1d2f52c7515684c3a78f8
tamitamboommm!! <pun.hoo.hoo@gmail.com>
2021-02-16 12:24:22 +0700

    Initial get value from URL in graph.html

---
+++ frontend/peoplegraph.js
@@ -0,0 +1,31 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+function getUrlParam(parameter, defaultvalue){
+    var urlparameter = defaultvalue;
+    if(window.location.href.indexOf(parameter) > -1){
+        urlparameter = getUrlVars()[parameter];
+        }
+    return urlparameter;
+}
+
+
+
+function headname(roomname){
+    a = document.createElement("h2");
+    a.innerText = "People in room " + roomname;
+    return a
+}
+
+var roomid = getUrlParam('roomid', 'outside')
+console.log(roomid)
+
+var namer = headname(roomid)
+console.log(namer)
+head = document.querySelector("#Graphhead");
+head.appendChild(headname(roomid));
