566ba9afdf8d18754d01f097d45999c603534bba
KongtappV <Kongtapp.v@ku.th>
2021-02-18 18:49:48 +0700

    update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,36 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from datetime import date
+from json import dumps
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_group11:2grm46fn@158.108.182.0:2255/exceed_group11'
 mongo = PyMongo(app)
 
-myCollection = mongo.db.g11
+building = mongo.db.building
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return {"message": "Hello World!"}
+    a = "Hello world!"
+    return f"<h1>{a}</h1>"
+
+@app.route('/', methods=['GET'])
+def get_data():
+    building_data = building.find()
+    result = []
+    for data in building_data:
+        result.append({
+            "floor": data["floor"]
+        })
+    return {"data": result}
+
+@app.route('/', methods=['PUT'])
+def update_data():
+    data = request.json
+    
+    building.insert_one({"floor":1})
+
+    return {"message": "insert complete!"}
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
