3d4ac79d788d54959dd1d548db9d6ab8015b8407
KongtappV <Kongtapp.v@ku.th>
2021-02-18 20:09:42 +0700

    update get

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from datetime import date
-from json import dumps
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://exceed_group11:2grm46fn@158.108.182.0:2255/exceed_group11'
@@ -9,19 +8,51 @@ mongo = PyMongo(app)
 
 building = mongo.db.building
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    a = "Hello world!"
-    return f"<h1>{a}</h1>"
+@app.route('/hardware', methods=['GET'])
+def get_hardware():
+
+    data = {
+        "room_id": 201,
+        "capacity": 20,
+        "current" : 10,
+        "mode": 0
+        }
+
+    return {"data": data}
+
+@app.route('/hardware', methods=['POST'])
+def post_hardware():
+    data = request.json
+    
+    filt = {"room_id": data["room_id"]}
+    query = building.find(filt)
+    query["current"] += data["status"]
+
+    
+    if query["current"] < 0:
+        query["current"] = 0
+    
+    # {
+    # room_id: 201,
+    # status: 0 (no activities), 1 (in คนเข้า), -1 (out คนออก),
+    # light: 0,
+    # alert: 0,
+    # temp: 25 
+    # }
+
+    output = []
+    return {"output": output}
 
 @app.route('/', methods=['GET'])
 def get_data():
     building_data = building.find()
     result = []
-    for data in building_data:
-        result.append({
-            "floor": data["floor"]
-        })
+    for build in building_data:
+        for data in build["building"]:
+            result.append({
+                "floor_number": data["floor_number"],
+                "rooms": data["rooms"]
+            })
     return {"data": result}
 
 @app.route('/', methods=['PUT'])
@@ -31,6 +62,7 @@ def update_data():
     building.insert_one({"floor":1})
 
     return {"message": "insert complete!"}
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
