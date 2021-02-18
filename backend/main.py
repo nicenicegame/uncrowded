dfbab5e85a91aa3bd98c3032cbed83228915469b
Nice <nicenicegame@gmail.com>
2021-02-18 19:02:48 +0700

    added user signin

--- backend/main.py
+++ backend/main.py
@@ -8,11 +8,14 @@ app.config['MONGO_URI'] = 'mongodb://exceed_group11:2grm46fn@158.108.182.0:2255/
 mongo = PyMongo(app)
 
 building = mongo.db.building
+users = mongo.db.users
+
 
 @app.route('/hello', methods=['GET'])
 def hello():
     a = "Hello world!"
     return f"<h1>{a}</h1>"
+
 
 @app.route('/', methods=['GET'])
 def get_data():
@@ -24,13 +27,21 @@ def get_data():
         })
     return {"data": result}
 
+
 @app.route('/', methods=['PUT'])
 def update_data():
     data = request.json
-    
-    building.insert_one({"floor":1})
+
+    building.insert_one({"floor": 1})
 
     return {"message": "insert complete!"}
+
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    user_data = request.json
+    user = users.find_one({'username': user_data['username']})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='3000', debug=True)
